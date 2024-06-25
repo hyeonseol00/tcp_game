7ed4a3945671430236e26d1a7ad1cdcb0305388a
hyeonseol00 <hyeonseol22@naver.com>
2024-06-25 17:18:28 +0900

    ♻️ refactor : 소켓 이벤트 분리

--- src/server.js
+++ src/server.js
@@ -1,26 +1,9 @@
 import net from "net";
 import initServer from "./init/index.js";
 import { config } from "./config/config.js";
+import { onConnection } from "./events/onConnection.js";
 
-const server = net.createServer((socket) =>
-{
-	console.log(`클라이언트가 연결되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
-
-	socket.on("data", (data) =>
-	{
-		console.log(data);
-	});
-
-	socket.on("end", () =>
-	{
-		console.log(`클라이언트 연결이 해제되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
-	});
-
-	socket.on("error", (err) =>
-	{
-		console.log(`소켓에서 에러가 발생했습니다: ${err}`);
-	});
-});
+const server = net.createServer(onConnection);
 
 initServer().then(() =>
 {
