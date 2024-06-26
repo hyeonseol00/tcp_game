69499fbb0385bf3330983fbd06e36c0c486d12eb
hyeonseol00 <hyeonseol22@naver.com>
2024-06-26 12:49:29 +0900

    ✨ update : 클라이언트에 반환하는 값 추가

--- src/handlers/user/initial.hander.js
+++ src/handlers/user/initial.hander.js
@@ -6,8 +6,16 @@ const initialHandler = async ({ socket, userId, payload }) =>
 
 	addUser(socket, deviceId);
 
+	// 유저 정보 응답 생성
+	const initialResponse = createResponse(
+		HANDLER_IDS.INITIAL,
+		RESPONSE_SUCCESS_CODE,
+		{ userId: deviceId },
+		deviceId,
+	);
+
 	// 소켓을 통해 클라이언트에게 응답 메시지 전송
-	socket.write("");
+	socket.write(initialResponse);
 };
 
 export default initialHandler;
