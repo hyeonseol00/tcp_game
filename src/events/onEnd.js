e4b69bdfb81fdff780bf8ed5426810c482508eda
hyeonseol00 <hyeonseol22@naver.com>
2024-06-26 20:36:35 +0900

    🩹 fix : 에러 유발 버그 수정

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,5 +1,7 @@
+import { removeUser } from "../session/user.session.js";
+
 export const onEnd = (socket) => () =>
 {
-	console.log('클라이언트 연결이 종료되었습니다.');
+	console.log('클라이언트 연결이 종료되었습니다:', socket.remoteAddress, socket.remotePort);
 	removeUser(socket);
 };
