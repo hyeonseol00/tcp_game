96d8aedcad91f90fe0764442f328b5ce9404183f
hyeonseol00 <hyeonseol22@naver.com>
2024-06-27 11:08:56 +0900

    🎉 add : 유저, 게임 정보 클래스 추가

---
+++ src/classes/models/game.class.js
@@ -0,0 +1,50 @@
+const MAX_PLAYERS = 2;
+
+class Game
+{
+	constructor(id)
+	{
+		this.id = id;
+		this.users = [];
+		this.state = 'waiting'; // 'waiting', 'inProgress'
+	}
+
+	addUser(user)
+	{
+		if (this.users.length >= MAX_PLAYERS)
+		{
+			throw new Error('게임 세션이 꽉 찼습니다!');
+		}
+		this.users.push(user);
+
+		if (this.users.length === MAX_PLAYERS)
+		{
+			setTimeout(() =>
+			{
+				this.startGame();
+			}, 3000);
+		}
+	}
+
+	getUser(userId)
+	{
+		return this.users.find((user) => user.id === userId);
+	}
+
+	removeUser(userId)
+	{
+		this.users = this.users.filter((user) => user.id !== userId);
+
+		if (this.users.length < MAX_PLAYERS)
+		{
+			this.state = 'waiting';
+		}
+	}
+
+	startGame()
+	{
+		this.state = 'inProgress';
+	}
+}
+
+export default Game;
